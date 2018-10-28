69c423eae84c57d9b43a95ade870273db5fe50fb
gforest_shade <shinapoka@gmail.com>
2018-10-29 06:29:45 +0900

    デプロイ用gh-pagesの操作にworktreeを使うように

--- pub.sh
+++ pub.sh
@@ -1,3 +1,7 @@
-rm -r docs
-bin/hugo && cd docs && git add --all && git commit -m "Update documentation" && cd ..
+rm -r docs/*
+bin/hugo
+cd docs
+git add .
+git commit -m "Update documentation"
+cd ..
 git push origin gh-pages
