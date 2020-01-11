629edd3bb80a97b2b411dc8184d8edef619f709d
gforest_shade <shinapoka@gmail.com>
2020-01-11 17:53:21 +0900

    デプロイスクリプト更新

--- pub.sh
+++ pub.sh
@@ -1,7 +1,28 @@
-rm -r docs/*
+#!/bin/sh
+
+if [ "`git status -s`" ]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
+
+echo "Deleting old publication"
+rm -rf docs
+mkdir docs
+git worktree prune
+rm -rf .git/worktrees/docs/
+
+echo "Checking out gh-pages branch into public"
+git worktree add -B gh-pages docs upstream/gh-pages
+
+echo "Removing existing files"
+rm -rf docs/*
+
+echo "Generating site"
 bin/hugo
-cd docs
-git add .
-git commit -m "Update documentation"
-cd ..
-git push origin gh-pages
+
+echo "Updating gh-pages branch"
+cd docs && git add --all && git commit -m "Update Documentation"
+
+#echo "Pushing to github"
+#git push origin gh-pages
